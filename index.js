8cd1b7578f8b54da91aa217c29f93de8a058e88a
tanjirman <tanjirmahbub07@gmail.com>
2026-05-21 09:28:34 +0600

    Adding available-tutor API

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
-const port = process.env.PORT || 5000 ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
-
-app.use(cors())
+app.use(cors());
 dotenv.config();
-
-
 
 const uri = process.env.MONGODB_URI;
 
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,26 +27,31 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    const db = client.db ("MediQueue")
-    const tutorsCollection = db.collection("Tutors")
+    const db = client.db("MediQueue");
+    const tutorsCollection = db.collection("Tutors");
 
-    app.get ("/tutors", async(req, res) =>{
+    app.get("/tutors", async (req, res) => {
       const cursor = tutorsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get ("/tutors/:tutorsId", async(req, res) =>{
-      const {tutorsId} = req.params;
-      const query = {_id: new ObjectId(tutorsId)};
+    app.get("/tutors/:tutorsId", async (req, res) => {
+      const { tutorsId } = req.params;
+      const query = { _id: new ObjectId(tutorsId) };
       const result = await tutorsCollection.findOne(query);
-      res.send(result)
+      res.send(result);
+    });
 
-    })
+    app.get("/featured-tutors", async (req, res) => {
+      const result = await tutorsCollection.find().limit(6).toArray();
 
+      res.send(result);
+    });
 
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,12 +59,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
