916028e55c0805cff14c064b3063a26c48be31fd
tanjirman <tanjirmahbub07@gmail.com>
2026-05-22 23:33:14 +0600

    update auth-js

--- auth.js
+++ auth.js
@@ -2,7 +2,13 @@ const { betterAuth } = require("better-auth");
 
 const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
+
   baseURL: process.env.BETTER_AUTH_URL,
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://medi-queue-zeta.vercel.app",
+  ],
 });
 
 module.exports = { auth };
