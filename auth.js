ef0e1f881e5706cc0748e3253bc5ace42ee2fec1
tanjirman <tanjirmahbub07@gmail.com>
2026-05-22 23:46:46 +0600

    update vercel.json

--- auth.js
+++ auth.js
@@ -1,14 +1,19 @@
 const { betterAuth } = require("better-auth");
+const { mongodbAdapter } = require("better-auth/adapters/mongodb");
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB_URI);
 
 const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
-
   baseURL: process.env.BETTER_AUTH_URL,
 
   trustedOrigins: [
     "http://localhost:3000",
     "https://medi-queue-zeta.vercel.app",
   ],
+
+  database: mongodbAdapter(client.db("MediQueue")),
 });
 
 module.exports = { auth };
